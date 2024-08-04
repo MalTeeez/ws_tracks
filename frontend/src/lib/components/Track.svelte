2a1fa2d8dc07ff8cf7d013f119f507e53db77cea
MalTeeez <41554385+MalTeeez@users.noreply.github.com>
2024-08-04 02:14:17 +0200

    Fixed dot shadow and relative pos

--- frontend/src/lib/components/Track.svelte
+++ frontend/src/lib/components/Track.svelte
@@ -13,18 +13,21 @@
 </script>
 
 <div class="absolute" style="top: {y}px; left: {x}px;">
-	<div class="plane size-2">
+	<div class="dot size-2">
 	</div>
-	<div class="antialiased font-mono text-xs font-semibold text-center text-sky-400">
+	<div class="antialiased font-mono text-xs font-semibold text-left text-sky-400">
 		{@render children()}
 	</div>
 </div>
 
 <style>
 
-    .plane {
+    .dot {
         background: yellow;
         border-radius: 4em;
+		box-shadow: 2px 2px 4px 1px rgba(0, 0, 0, 0.4);
+		position: relative;
+		left: 25%;
     }
 
 </style>
